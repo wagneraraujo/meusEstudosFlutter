64a1508b783dbf1a11cd0b323b630d364a177acb
Wagner Araújo <wagneraraujo15@gmail.com>
2020-01-10 07:20:56 -0300

    list

---
+++ list_flutter/lib/Home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _itens = [];
+
+  void _carregarItens() {
+    for (int i = 0; i <= 10; i++) {
+      Map<String, dynamic> item = Map();
+      item["titulo"] = "Título ${i} teste lista";
+      item["Descrição"] = "descricao ${i} teste lista";
+      _itens.add(item);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _carregarItens();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listas com Flutter"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: _itens.length,
+          itemBuilder: (context, indice) {
+            // Map<String, dynamic> item = _itens[indice];
+            // print("item ${item["titulo"]}");
+            return ListTile(
+              title: Text(_itens[indice]["titulo"]),
+              subtitle: Text(_itens[indice]["Descrição"]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
