b61d9b9e4517916c42f89c3a938fcd2d2cff5e8c
Wagner Araújo <wagneraraujo15@gmail.com>
2020-01-14 19:30:36 -0300

    botoes de navegaçao no rodape do app

--- youtube/lib/Home.dart
+++ youtube/lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricoes.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -8,15 +12,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Biblioteca(),
+      Inscricoes(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
           color: Colors.redAccent,
         ),
         backgroundColor: Colors.white,
-        title: Image.asset("imagens/youtube.png"),
+        title: Image.asset(
+          "imagens/youtube.png",
+          height: 22,
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
@@ -38,7 +53,42 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.red,
+
+        //muda apenas a cor da fonte e icone
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _indiceAtual,
+        //mudança automatica ao escolher outro menu, mdança de cor
+        onTap: (indice) {
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Colors.red,
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.redAccent,
+            icon: Icon(Icons.whatshot),
+            title: Text('Em Alta'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.orangeAccent,
+            icon: Icon(Icons.bookmark),
+            title: Text('Biblioteca'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blueAccent,
+            icon: Icon(Icons.school),
+            title: Text('Inscrições'),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ youtube/lib/telas/Inscricoes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Inscricoes extends StatefulWidget {
+  Inscricoes({Key key}) : super(key: key);
+
+  @override
+  _InscricoesState createState() => _InscricoesState();
+}
+
+class _InscricoesState extends State<Inscricoes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Inscricoes", style: TextStyle(fontSize: 30))),
+    );
+  }
+}
