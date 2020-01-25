176baba7f224128705fd674826c7dfd1b46ad1e4
Wagner Araújo <wagneraraujo15@gmail.com>
2020-01-25 08:29:52 -0300

    youtube

--- youtube/lib/Home.dart
+++ youtube/lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/CustomSearchDelegate.dart';
 import 'package:youtube/telas/Biblioteca.dart';
 import 'package:youtube/telas/EmAlta.dart';
 import 'package:youtube/telas/Inicio.dart';
@@ -10,40 +11,61 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   int _indiceAtual = 0;
+  String _resultado = "";
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> telas = [Inicio(), EmAlta(), Inscricao(), Biblioteca()];
+
+    List<Widget> telas = [
+      Inicio( _resultado ),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca()
+    ];
 
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.grey),
+        iconTheme: IconThemeData(
+          color: Colors.grey
+        ),
         backgroundColor: Colors.white,
         title: Image.asset(
-          "imagens/youtube.png",
+            "imagens/youtube.png",
           width: 98,
           height: 22,
         ),
         actions: <Widget>[
+
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              String res = await showSearch(context: context, delegate: CustomSearchDelegate());
+              setState(() {
+                _resultado = res;
+              });
+              print("resultado: digitado " + res );
+            },
+          ),
+
+          /*
           IconButton(
             icon: Icon(Icons.videocam),
-            onPressed: () {
+            onPressed: (){
               print("acao: videocam");
             },
           ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print("acao: pesquisa");
-            },
-          ),
+
           IconButton(
             icon: Icon(Icons.account_circle),
-            onPressed: () {
+            onPressed: (){
               print("acao: conta");
             },
           )
+          */
+
+
         ],
       ),
       body: Container(
@@ -51,32 +73,37 @@ class _HomeState extends State<Home> {
         child: telas[_indiceAtual],
       ),
       bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _indiceAtual,
-          onTap: (indice) {
-            setState(() {
-              _indiceAtual = indice;
-            });
-          },
-          type: BottomNavigationBarType.fixed,
-          fixedColor: Colors.red,
+        currentIndex: _indiceAtual,
+        onTap: (indice){
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
           items: [
             BottomNavigationBarItem(
-                //backgroundColor: Colors.orange,
-                title: Text("Início"),
-                icon: Icon(Icons.home)),
+              //backgroundColor: Colors.orange,
+              title: Text("Início"),
+              icon: Icon(Icons.home)
+            ),
             BottomNavigationBarItem(
                 //backgroundColor: Colors.red,
                 title: Text("Em alta"),
-                icon: Icon(Icons.whatshot)),
+                icon: Icon(Icons.whatshot)
+            ),
             BottomNavigationBarItem(
                 //backgroundColor: Colors.blue,
                 title: Text("Inscrições"),
-                icon: Icon(Icons.subscriptions)),
+                icon: Icon(Icons.subscriptions)
+            ),
             BottomNavigationBarItem(
                 //backgroundColor: Colors.green,
                 title: Text("Biblioteca"),
-                icon: Icon(Icons.folder)),
-          ]),
+                icon: Icon(Icons.folder)
+            ),
+          ]
+      ),
     );
   }
 }
--- youtube/lib/main.dart
+++ youtube/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'Home.dart';
+import 'package:youtube/Home.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
+
